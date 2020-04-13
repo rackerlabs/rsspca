25ac60689a2219ab5cb1a2b0c8d048e4a920a6f5
Bruce Stringer <bruce.stringer@rackspace.com>
2020-04-13 17:13:55 -0500

    various updates

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-circleci local execute --job orb-tools/lint && \
-circleci local execute --job orb-tools/shellcheck && \
 circleci config pack src > orb.yml && \
 circleci orb publish orb.yml rackerlabs/rsspca@dev:alpha && rm -rf orb.yml
 
